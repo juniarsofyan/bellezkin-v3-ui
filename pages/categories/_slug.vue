8768c7c760efc856c49a39e51f306557b723c276
juniarsofyan <choerulsofyanmf@gmail.com>
2020-07-11 15:09:40 +0700

    Part 07 Showing categories and children

---
+++ pages/categories/_slug.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique sint iure molestiae ipsam soluta, voluptates sunt dolores suscipit hic amet accusamus animi quod reiciendis consequuntur quibusdam, fugiat nemo id corporis!
+    </p>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
